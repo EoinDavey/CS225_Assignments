c52a8aeb1f5cf3d6f009abe35672862f626a6e97
Eoin Davey <eoind@vey.ie>
2016-10-15 01:16:21 +0100

    Another attempt at q2

--- Assignment2/ans.hs
+++ Assignment2/ans.hs
@@ -25,7 +25,7 @@ map' f as = foldl (\z x -> z++[(f x)]) [] as
 --------------------------------------------------
 -- TypeClasses
 -- q1: N={1,2,..} cannot be a monoid set under addition because the addition identity element (zero) is not in the set N.
--- q2: End(A) cannot be guaranteed to form a monoid as not enough is known about the set A to define any operations on it
+-- q2: End(A) can form a monoid under the composition operation, the identity of the composition function is just e(x)->x, e.f(x) = f.e(x) = f(x)
 -- q3:
 
 data Failable a = Failure String | OK a
